c604cda9862592d807fe386cbbb93350c2e04bfc
Rémi DUFEU <r.dufeu@dso.fr>
2022-08-10 11:23:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/reglages', (req, res) => {
+    let state = {
+        "power": true,
+        "level": 0.5,
+        "currentMode": "COSY",
+        "modes": [
+            {"name": "COSY", "streaming": false, "color" : ["rgb(255, 0, 0)"]},
+            {"name": "COLOR", "streaming": false, "color" : ["rgb(180, 0, 90)"]}
+        ]
+    }
+  res.send(state)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
